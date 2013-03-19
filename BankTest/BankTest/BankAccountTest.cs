cc56b957a7f4eb8eaeb1044a6c8be3438d50283a
vitobudiman <nyx23@msn.com>
2013-03-19 14:25:34 +0700

    First commit

---
+++ BankTest/BankTest/BankAccountTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+
+namespace BankTest
+{
+    [TestClass]
+    public class BankAccountTest
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            // assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+        //unit test method
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            // assert is handled by ExpectedException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Account_NotExist()
+        {
+            double debitAmount = 100.00;
+
+            BankAccount account = new BankAccount();
+
+            account.Debit(debitAmount);
+        }
+    }
+}
